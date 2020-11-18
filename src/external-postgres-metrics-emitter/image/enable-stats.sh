71e578c723dd91ab48025b0de57c37e46965980f
rkoster <rkoster@starkandwayne.com>
2020-11-18 10:23:03 +0100

    Create extension on db init

--- src/external-postgres-metrics-emitter/image/enable-stats.sh
+++ src/external-postgres-metrics-emitter/image/enable-stats.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -e
+
 echo "shared_preload_libraries = 'pg_stat_statements'" >> $PGDATA/postgresql.conf
 echo "pg_stat_statements.max = 10000" >> $PGDATA/postgresql.conf
 echo "pg_stat_statements.track = all" >> $PGDATA/postgresql.conf
+
+"${psql[@]}" --dbname="$POSTGRES_DB" <<-'EOSQL'
+	CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+
