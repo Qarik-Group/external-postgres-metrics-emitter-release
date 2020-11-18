623ea181c5cffedc70a6d0ca07560a5178ccbb41
rkoster <rkoster@starkandwayne.com>
2020-11-18 13:18:19 +0100

    Added remaining fields to StatementStat

--- src/external-postgres-metrics-emitter/postgres/types.go
+++ src/external-postgres-metrics-emitter/postgres/types.go
@@ -11,32 +11,29 @@ type Client struct {
 	db *sql.DB
 }
 
-// userid              | oid              |           |          |
-// dbid                | oid              |           |          |
-// queryid             | bigint           |           |          |
-// query               | text             |           |          |
-// calls               | bigint           |           |          |
-// total_time          | double precision |           |          |
-// min_time            | double precision |           |          |
-// max_time            | double precision |           |          |
-// mean_time           | double precision |           |          |
-// stddev_time         | double precision |           |          |
-// rows                | bigint           |           |          |
-// shared_blks_hit     | bigint           |           |          |
-// shared_blks_read    | bigint           |           |          |
-// shared_blks_dirtied | bigint           |           |          |
-// shared_blks_written | bigint           |           |          |
-// local_blks_hit      | bigint           |           |          |
-// local_blks_read     | bigint           |           |          |
-// local_blks_dirtied  | bigint           |           |          |
-// local_blks_written  | bigint           |           |          |
-// temp_blks_read      | bigint           |           |          |
-// temp_blks_written   | bigint           |           |          |
-// blk_read_time       | double precision |           |          |
-// blk_write_time      | double precision |           |          |
 type StatementStat struct {
-	Query   string `sql:"query"`
-	QueryID int    `sql:"queryid"`
-	Calls   int    `sql:"calls"`
 	Timestamp         time.Time `sql:"-"`
+	UserID            int
+	DbID              int
+	QueryID           int64
+	Query             string
+	Calls             int64
+	TotalTime         float64 `sql:"total_time"`
+	MinTime           float64 `sql:"min_time"`
+	MaxTime           float64 `sql:"max_time"`
+	MeanTime          float64 `sql:"mean_time"`
+	StdDevTime        float64 `sql:"stddev_time"`
+	Rows              int64
+	SharedBlksHit     int64   `sql:"shared_blks_hit"`
+	SharedBlksRead    int64   `sql:"shared_blks_read"`
+	SharedBlksDirtied int64   `sql:"shared_blks_dirtied"`
+	SharedBlksWritten int64   `sql:"shared_blks_written"`
+	LocalBlksHit      int64   `sql:"local_blks_hit"`
+	LocalBlksRead     int64   `sql:"local_blks_read"`
+	LocalBlksDirtied  int64   `sql:"local_blks_dirtied"`
+	LocalBlksWritten  int64   `sql:"local_blks_written"`
+	TempBlksRead      int64   `sql:"temp_blks_read"`
+	TempBlksWritten   int64   `sql:"temp_blks_written"`
+	BlkReadTime       float64 `sql:"blk_read_time"`
+	BlkWriteTime      float64 `sql:"blk_write_time"`
 }
